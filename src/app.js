7f5faeb182b68a05ed5ca4502ba0147af5870a70
unknown <vamsi3353@gmail.com>
2020-07-26 21:27:58 +0530

    vamsi

--- src/app.js
+++ src/app.js
@@ -1,59 +1,83 @@
 const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
-    "Slingo"
+  "Spiderman",
+  "Captain America",
+  "Wonderwoman",
+  "Popcorn",
+  "Gemwoman",
+  "Bolt",
+  "Antwoman",
+  "Mask",
+  "Tiger",
+  "Captain",
+  "Catwoman",
+  "Fish",
+  "Hulk",
+  "Ninja",
+  "Black Cat",
+  "Volverine",
+  "Thor",
+  "Slayer",
+  "Vader",
+  "Slingo",
 ];
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+  let detailedPlayers = "";
+  detailedPlayers = players.map((value, index) => ({
+    name: players[index],
+    strength: getRandomStrength(),
+    image: "./images/super-" + (index + 1) + ".png",
+    type: "hero|villian",
+  }));
+  // Instead of forloop use Map method
+  // Code here
 
-    // Instead of forloop use Map method
-    // Code here
-
-    return detailedPlayers;
-}
+  return detailedPlayers;
+};
 
 // getting random strength
 const getRandomStrength = () => {
-    return Math.ceil(Math.random() * 100);
-}
+  return Math.ceil(Math.random() * 100);
+};
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+  let fragment = "";
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
+  // Instead of using for loop
+  // Use chaining of Array methods - filter, map and join
+  // Type your code here
+  var indexList = [];
+  if (type == "hero") {
+    players.map((item, index) => {
+      if (index % 2 == 0) indexList.push(index);
+    });
+  } else {
+    players.map((item, index) => {
+      if (index % 2 != 0) indexList.push(index);
+    });
+  }
 
-    return fragment;
-}
+  fragment += indexList.map(
+    (index) => `<div class="player">
+             <img src="${players[index].image}" alt="">
+             <div class="name">${players[index].name}</div>
+             <div class="strength">${players[index].strength}</div></div>`
+  );
+
+  return fragment;
+};
 
 // Display players in HTML
 const viewPlayers = (players) => {
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-}
-
+  document.getElementById("heroes").innerHTML = buildPlayers(players, "hero");
+  document.getElementById("villains").innerHTML = buildPlayers(
+    players,
+    "villain"
+  );
+};
 
 window.onload = () => {
-    viewPlayers(initPlayers(PLAYERS));
-}
+  viewPlayers(initPlayers(PLAYERS));
+};
